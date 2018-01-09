36bcb9e774690df81a9e354322d49febd48719e4
Ermias Asmelash <fromaddistohelsinki@gmail.com>
2018-01-09 17:31:26 +0200

    Todo started

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {id: 1, name: 'Learn JSX', isComplete: false},
+        {id: 1, name: 'Learn JS', isComplete: false},
+        {id: 1, name: 'Learn CSS', isComplete: false}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Todos</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div className="Todo-App">
+        <form>
+          <input type="text" />
+        </form>
+        <div>
+          <ul>
+            {
+              this.state.todos.map(function(todo) {
+                return (
+                <li>
+                  <input key={todo.id} type="checkbox" />
+                  {todo.name}
+                  </li>)
+              })
+            }
+          </ul>
+        </div>
+       </div>
       </div>
     );
   }
